b01fa3dd9d7369adbe880540da4831491af16e63
gityeldho <yeldhomathai0608@gmail.com>
2025-04-07 21:16:05 +0530

    Initial commit - backend

--- server.js
+++ server.js
@@ -135,5 +135,5 @@ app.post("/predict", async (req, res) => {
 });
 
 // ✅ Start Server for Deployment
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
